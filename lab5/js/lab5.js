0518ef34d73a9bc6d9e18fdef5cd2938159b85c2
tatumgoing <tatumgoing@yahoo.com>
2021-10-18 14:26:33 -0700

    Lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Tatum Going
+ * Created:   10.18.2020
+ *
+ * License: Public Domain.
+ **/
+
+ //Define variables
+ make= "Toyota";
+ model= "Rav 4";
+ color= "White";
+ year= 2015;
+
+ //calculate
+ age= 2021-year;
+
+ //output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "<br>");
